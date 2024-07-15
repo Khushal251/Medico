d2c1333d04326c7a85a5302b068ff4affb995550
Khushal Kumawat <kumawatkhushal251@gmail.com>
2024-07-16 01:26:41 +0530

    clerkclient error removed

--- middleware.ts
+++ middleware.ts
@@ -10,12 +10,35 @@
 //   publicRoutes: ['/api/webhook/clerk']
 // }
 
-import { authMiddleware } from "@clerk/nextjs/server"; 
 
-export default authMiddleware({
-  publicRoutes: ['/api/webhooks/clerk']
-});
+// AS PER THE PROJECT - VIDEO
+// import { authMiddleware } from "@clerk/nextjs/server"; 
+
+// export default authMiddleware({
+//   publicRoutes: ['/api/webhooks/clerk']
+// });
+
+// export const config = {
+//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)","/","/(api|trpc)(.*)"],
+// }
+
+
+
+// AS PER DOCUMENTATION - CHAT GPT
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+// Define the public routes
+const publicRoutes = createRouteMatcher(['/api/webhooks/clerk']);
+
+export default clerkMiddleware();
+
+// export default clerkMiddleware((auth, req) => {
+//   // Protect all routes except the public ones
+//   if (!publicRoutes(req)) {
+//     auth().protect();
+//   }
+// });
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)","/","/(api|trpc)(.*)"],
-}
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
